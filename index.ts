c7ad52b62c33597b1390783fc9f7c2c899b4a4c7
Darin Pope <darin@planetpope.com>
2021-10-25 16:34:54 -0400

    change name
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import * as linode from "@pulumi/linode";
 const primary = linode.getObjectStorageCluster({
     id: "us-east-1",
 });
-const foobar = new linode.ObjectStorageBucket("foobar", {
+const foobar = new linode.ObjectStorageBucket("darin-c088c595", {
     cluster: primary.then(primary => primary.id),
-    label: "foobar",
+    label: "darin-c088c595",
 });
