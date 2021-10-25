ab3cb9798f257a3255d28f5ffe27df2a7d901e1b
Darin Pope <darin@planetpope.com>
2021-10-25 16:19:25 -0400

    linode
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as linode from "@pulumi/linode";
+
+const primary = linode.getObjectStorageCluster({
+    id: "us-east-1",
+});
+const foobar = new linode.ObjectStorageBucket("darin-1b9psw1s", {
+    cluster: primary.then(primary => primary.id),
+    label: `%s`,
+});
